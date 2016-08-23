751705a743c66295159a076552b505aa95b38356
Nathan Hourt <nat.hourt@gmail.com>
2016-08-23 17:16:26 -0500

    Initial commit

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+## Clone graphene repo and submodules
+git clone https://github.com/followmyvote/graphene --recursive
+cd graphene
+
+# Configure build
+cmake . -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/var/fmv/local -G Ninja
+
+# Build and install
+ninja
+ninja install
